948e777edc78e14f1ea5c8af1104871289b548d6
PakoLinux <pakolinux@gmail.com>
2023-06-27 20:21:07 +0200

    actualización proyecto

--- index.jsx
+++ index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 
-ReactDOM.render(<h1>My Wishlist from REACT</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <div>My Wishlist from REACT</div>,
+    document.getElementById('root')
+);
